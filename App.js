550e43478d398df167f8b8c74ddb07bfd68b2fd2
Kelvin Jones <kevinjones4453@gmail.com>
2019-01-15 02:32:45 -0500

    testing

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+         <Image
+          style={styles.img}
+          source={{uri: 'http://vrcpitbull.com/wp-content/uploads/2011/02/VAMPVAMP%E2%80%A2.jpg'}}
+        />
         <Text>THIS IS THE COOLEST APP EVER!!!</Text>
+        <Text>DEVELOPERS!!DEVELOPERS!!DEVELOPERS!!</Text>
       </View>
     );
   }
@@ -18,4 +23,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img: {
+    height: 200,
+    width: 200
+  }
 });
